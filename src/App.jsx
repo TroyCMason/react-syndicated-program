c2b26530258f9e37ce1b342b230e10ccfc8e1ffd
TroyCMason <troymason222@gmail.com>
2023-07-26 15:05:14 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './Components/Todo.jsx'
+import Title from './Components/Title.jsx'
+
+function App() {
+  return (
+    <div>
+     <Title/>
+     <div className='todo__wrapper'>
+
+     <Todo/>
+     <Todo/>
+     <Todo/>
+     </div>
+     </div>
+  );
+}
+
+export default App;
+pp
