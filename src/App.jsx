3166a30c978a64fc6266121f7e97e075ec4177b6
TroyCMason <troymason222@gmail.com>
2023-07-31 05:19:21 -0400

    Added Routing Notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,22 @@
 import './App.css';
-import Todo from './Components/Todo.jsx'
-import Title from './Components/Title.jsx'
-import Modal from './Components/Modal';
-import React, {useState, useEffect} from 'react';
-import Counter from "./Components/Counter.jsx"
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import Home from './Pages/Home.jsx';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Nav from './Components/nav'
 
 function App() {
-const [showModal, setShowModal] = useState(false);
-
-function onToDoDelete(){
-  setShowModal(true);
-}
-
-function cancelModal() {
-  setShowModal(false);
-}
-
-function confirmModal(){
-  setShowModal(false);
-}
-
-useEffect(()=> {
-  console.log("ONLY on mount")
-  setShowModal(!showModal)
-}, [])
-
-useEffect (()=>{
-  console.log('on mount AND on ${showModal} change')
-}, [showModal])
-
-useEffect(() => {
-  console.log ('EVERY render')
-  // setShowModal(!showModal)
-})
 
   return (
-    <>
-     <Title/>
-     <div>
-      <input type="text" onChange={(event)=>{
-      console.log(event.target.value)
-      }}/>
-        <button onClick ={()=> setShowModal(true)}>Add todo</button>
-     </div>
-     <div className='todo__wrapper'>
-     <Todo onToDoDelete={onToDoDelete} title="Finish Frontend Simplified"
-     paragraph="Code along with Frontend Simplified"/>
-     <Todo title="Complete Interview Section"
-     paragraph="Finish every interview question"
-     />
-    <Todo onToDoDelete={onToDoDelete}  title="Land a 100k a year job"
-     paragraph="Apply to 100 jobs"/>
-     <Todo title="Pay off Debt"/>
-     <Todo title="Pay off Taxes" />
-     <Todo title="Save my Family" />
-     </div>
-     {false && <Modal title="Confirm Delete?"/>}
-     </>
+     <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/users/:name" elements = {<Users/>} />
+          </Routes>
+     </Router>
   );
 }
 
---
+++ src/Pages/user.jsx
@@ -0,0 +1,5 @@
+function User(){
+return <h1>Users</h1>
+}
+
+export default Users
