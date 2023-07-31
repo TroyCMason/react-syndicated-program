acbb21bde6d2a94267db2148d7075511025ef7e9
TroyCMason <troymason222@gmail.com>
2023-07-30 20:31:46 -0400

    update

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,33 +1,23 @@
 import React, { useState } from 'react';
 import './Counter.css'
 
-function Counter () {
-   const[cart, setCart] = useState ({
- item: " apple",
- quantity: 0
-   });
+   function Counter(){
+const [arr,setArr] = useState([])
 
-function removeApple () {
-    setCart((prevCart) => ({
-        ...prevCart,
-        quatity : prevCart.cart - 1
-    }))
+function addPlus () {
+setArr(prevArr => [...prevARR, "+"])
 }
 
-   function addApple(){
-    setCart(prevCart => ({
-        ...prevCart,
-        quantity: prevCart.quantity + 1,
-    }))
-   }
+function addMinus (){
+setArr()
+}
+
     return (
         <div>
-            <button onClick={removeApple}>-</button>
-            {cart.quatity}
-            {cart.item}
-            <button onClick={() => null}>+</button>
+            <button onClick={addMinus}>-</button>
+            <button onClick={addPlus}>+</button>
         </div>
-    ) 
-}
+    )};
 
-export default Counter
+
+export default Counter;
